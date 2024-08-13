7b9d6058d6de0530ac2b21f2b926a64e028d547e
Cyber-SADMAN <sadman2001.ssi@gmail.com>
2024-08-13 10:16:22 +0100

    Delete frontend directory

--- frontend/src/pages/doctors/Edit.tsx
+++
@@ -1,9 +0,0 @@
-const Edit = () => {
-    return (
-        <div>
-            <h1>Edit doctor info</h1>
-        </div>
-    );
-};
-
-export default Edit;
--- frontend/src/pages/doctors/List.tsx
+++
@@ -1,9 +0,0 @@
-const List = () => {
-    return (
-        <div>
-            <h1>Doctors list for amdin</h1>
-        </div>
-    );
-};
-
-export default List;
