d0d7b777e570f213f1f491f4dd8ea67f087b639c
Cyber-SADMAN <sadman2001.ssi@gmail.com>
2024-08-13 10:15:13 +0100

    Delete data-scrapper directory

--- data-scrapper/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const DB_LIVE_URL =
-    'mongodb+srv://mroot:pass9859@cluster-0.ntw5yfz.mongodb.net/docconnect';
-
-const instance = await mongoose.connect(DB_LIVE_URL);
-const db = instance.connection.db;
-
-export default db;
--- data-scrapper/index.js
+++
@@ -1,9 +0,0 @@
-import doctorInfoScrapper from './scrapDoctorInfo.js';
-// import insertData from './insertData.js';
-
-const main = async () => {
-    await doctorInfoScrapper();
-    // await insertData();
-};
-
-main();
